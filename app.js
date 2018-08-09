70f052aebb006420f6a9b733fc449ac380eed13f
Claudenir Freitas <claudenir.machado@sensedia.com>
2018-08-09 11:54:59 -0300

    fix configs

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-let fetchPayments = (paymentBody) => {
+let fetchPayments = paymentBody => {
     fetch("https://api-apix.sensedia.com/payment/v1/payments", {
-    method: 'POST',
-    headers: {
-        'Content-type':'application/json',
-        'Accept':'application/json'
-    },
-    mode: 'cors',
-    body: JSON.stringify(paymentBody)
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'Accept': 'application/json'
+        },
+        mode: 'cors',
+        body: JSON.stringify(paymentBody)
     })
-    .then(function(response) {
-    console.log(response);
-    return response;
-    })
-    .then(function(data) {
-    console.log(data);
-    })
-    .catch(function(err) {
-    console.log(err);
-    });
+        .then(function (response) {
+            return response;
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 
 }
 
 let fetchValidateBin = (binNumber) => {
- return    fetch("https://hml-api.elo.com.br/graphql", {
-    method: 'POST',
-    headers: {
-        'Content-type':'application/json',
-        'Accept':'application/json', 
-        "client_id":"8049be2c-d5f3-3478-985a-fd38cd7eed13"
-    },
-    body: JSON.stringify({"query":`query { bin (number:\"${binNumber}\"){ brand { name, image{url, width, height, mimeType}, url} } }`})
+    return fetch("<informar a url do desenvolvedores da Elo>", {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'Accept': 'application/json',
+            "client_id": "<informe a client_id do Portal de desenvolvedores da Elo>"
+        },
+        body: JSON.stringify({ "query": `query { bin (number:\"${binNumber}\"){ brand { name, image{url, width, height, mimeType}, url} } }` })
     })
-    .then(res => res.json().then(resp => resp)).catch(function(err) {
-    console.log(err);
-    });
+        .then(res => res.json().then(resp => resp))
+        .catch(function (err) {
+            console.log(err);
+        });
 
 }
